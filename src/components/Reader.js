c2c0bf4e83f12a892ac23f5a92541f552eba9692
Paulo Silva <paulosilva@Paulos-MacBook-Air.local>
2022-07-27 14:47:16 -0300

    inital commit

---
+++ src/components/Reader.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Reader() {
+  return (
+    <View>
+      <Text>Reader here!</Text>
+    </View>
+  );
+}
