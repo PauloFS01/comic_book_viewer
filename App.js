7e5656365ee8011a18c3bfaf984be4631ac30e14
Paulo Silva <paulosilva@Paulos-MacBook-Air.local>
2022-08-09 19:58:45 -0300

    create modal

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Reader from "./src/components/Reader";
+import { StyleSheet, Text, View, Modal, Button } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import Reader from "./src/components/Reader";
+import ReaderModal from "./src/components/ReaderModal";
+
 export default function App() {
-  // const insets = useSafeAreaInsets();
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <SafeAreaProvider>
-      <Reader />
+      <View style={styles.container}></View>
+      <ReaderModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
+      <Button
+        onPress={() => {
+          setModalVisible(!modalVisible);
+        }}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      {/* <Reader /> */}
       <StatusBar barStyle="light-content" backgroundColor={"#FFC700"} />
     </SafeAreaProvider>
   );
@@ -17,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: "#fff",
+    // backgroundColor: "green",
     // alignItems: "center",
     // justifyContent: "center",
   },
--- src/components/ReaderModal.js
+++ src/components/ReaderModal.js
@@ -1,10 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Modal, Button } from "react-native";
 import React from "react";
 
-export default function ReaderModal({ chieldren }) {
+export default function ReaderModal({ modalVisible, setModalVisible }) {
   return (
     <View style={styles.container}>
-      <Text>ReaderModal</Text>
+      <Modal
+        style={{ backgroundColor: "black", height: "100%" }}
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={{ backgroundColor: "black", height: "100%" }}>
+          <View>
+            <Text>ReaderModal</Text>
+            <Button
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+              title="close"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
